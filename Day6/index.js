cfb83bcb7a323a175a1faa8ace83123e773d8d0d
Calvin Goodale <c.goodale@compoundingsolutions.net>
2023-12-06 12:33:19 -0500

    Completed Day6 Part2

--- Day6/index.js
+++ Day6/index.js
@@ -4,8 +4,6 @@ var data = fs.readFileSync('input2.txt', 'utf8').toString();
 var splitInput = data.split(/\n/);
 
 // Part 1
-console.log(splitInput);
-
 var times = splitInput[0].split(': ')[1].trim().split(/\s+/);
 var distances = splitInput[1].split(': ')[1].trim().split(/\s+/);
 
@@ -21,7 +19,6 @@ for (var i = 0; i < times.length; i++) {
       // Time left in the race is time minus the held button time
       var timeLeft = time - heldms;
       var distanceTraveled = timeLeft * heldms;
-      console.log(`speed: ${heldms} | timeLeft: ${timeLeft} | distanceTraveled: ${distanceTraveled}`)
       if (distanceTraveled > distance) {
          winCount++;
       }
@@ -30,9 +27,35 @@ for (var i = 0; i < times.length; i++) {
 }
 
 var multiplied = 1;
-console.log(waysToWin);
 for (ways of waysToWin) {
    multiplied *= ways;
 }
 
 console.log(`answer: ${multiplied}`)
+
+// Part 2
+var data = fs.readFileSync('input2.txt', 'utf8').toString();
+var splitInput = data.split(/\n/);
+
+var time = splitInput[0].split(': ')[1].trim().split(/\s+/).join('');
+var distance = splitInput[1].split(': ')[1].trim().split(/\s+/).join('');
+
+var waysToWin = [];
+
+var distanceTraveled = 0;
+var winCount = 0;
+
+// For each number of ms to hold down button
+for (var heldms = 0; heldms < time; heldms++) {
+   // Time left in the race is time minus the held button time
+   var timeLeft = time - heldms;
+   var distanceTraveled = timeLeft * heldms;
+   // console.log(`speed: ${heldms} | timeLeft: ${timeLeft} | distanceTraveled: ${distanceTraveled}`)
+   if (distanceTraveled > distance) {
+      winCount++;
+   }
+}
+waysToWin.push(winCount);
+
+
+console.log(waysToWin);
